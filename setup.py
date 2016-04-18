29e77cd006ae50924fe9da03c2ce066c16f45da6
Daniel Fernandez <ekans_@hotmail.com>
2016-04-18 05:40:58 -0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='qqpat',
-      version='1.518',
+      version='1.519',
       description='Python Financial Performance Analysys Tool',
       classifiers=[
         'Development Status :: 3 - Alpha',
